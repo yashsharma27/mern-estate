e7e6309dfdba7915e7aa7e24daa93f3ce3f8c9b0
Yash Sharma <yash.s@decisionpoint.local>
2024-07-26 23:47:46 +0530

    backend for signup route created

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,9 +14,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(3000, ()=>{
     console.log('Server is running at port 3000')
 })
 
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
