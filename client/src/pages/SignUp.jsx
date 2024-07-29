54fefbf125f1c4df40627cd7c0ac8873497c0f1c
Yash Sharma <yash.s@decisionpoint.local>
2024-07-29 11:44:33 +0530

    ui for signup page created

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'></input>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'></input>
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'></input>
+        <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95'>SignUp</button>
+      </form>
     </div>
   )
 }
