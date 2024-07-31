70abac3136009505b494724790e85e439f6f5f1e
Yash Sharma <yash.s@decisionpoint.local>
2024-07-31 15:57:40 +0530

    signin functionality created

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -63,6 +63,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
