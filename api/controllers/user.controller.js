a06a612d5fd4d80c492315cc8601c1f659db6f9c
Yash Sharma <yash.s@decisionpoint.local>
2024-08-12 23:50:10 +0530

    update user functionality created

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,34 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res)=>{
     res.json({
         msg : "Api route is working",
     });
+}
+
+export const updateUser = async(req, res, next) => {
+    console.log(req.user.id)
+    console.log(req.params.id)
+    if(req.user.id !== req.params.id) return next(errorHandler('401', "Can only update your account"));
+    try {
+        if(req.body.password){
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username : req.body.username,
+                email : req.body.email,
+                password : req.body.password,
+                avatar : req.body.avatar
+            }
+        }, {new : true})
+
+        const {password, ...rest} = updatedUser._doc;
+
+        res.status(200).json(rest)
+    } catch (error) {
+        
+    }        
 }
