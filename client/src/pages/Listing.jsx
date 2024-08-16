fb3dcbfd09ba0468f40cc2fbbc85250cb4f36203
Yash Sharma <yash.s@decisionpoint.local>
2024-08-16 18:04:47 +0530

    contact landlord functionality done

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -15,7 +15,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-// import Contact from '../components/Contact';
+import Contact from '../components/Contact';
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -137,15 +137,15 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {/* {currentUser && listing.userRef !== currentUser._id && !contact && (
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
                 onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
               >
                 Contact landlord
               </button>
-            )} */}
-            {/* {contact && <Contact listing={listing} />} */}
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
