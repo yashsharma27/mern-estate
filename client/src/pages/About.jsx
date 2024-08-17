10e856d40f41a1309ca112ad1f015bd09b5d6281
Yash Sharma <yash.s@decisionpoint.local>
2024-08-17 22:35:06 +0530

    completed full project

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Yash Estate</h1>
+      <p1 className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, laborum ducimus totam incidunt inventore eaque laboriosam voluptatibus aperiam corrupti eum dicta saepe iusto unde, reprehenderit possimus ab ipsum iste mollitia odit aut quibusdam, ex modi optio! Impedit aliquid quam provident reprehenderit debitis facilis consequatur ipsam non ullam, commodi exercitationem quas!</p1>
+      <p1 className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, laborum ducimus totam incidunt inventore eaque laboriosam voluptatibus aperiam corrupti eum dicta saepe iusto unde, reprehenderit possimus ab ipsum iste mollitia odit aut quibusdam, ex modi optio! Impedit aliquid quam provident reprehenderit debitis facilis consequatur ipsam non ullam, commodi exercitationem quas!</p1>
+      <p1 className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, laborum ducimus totam incidunt inventore eaque laboriosam voluptatibus aperiam corrupti eum dicta saepe iusto unde, reprehenderit possimus ab ipsum iste mollitia odit aut quibusdam, ex modi optio! Impedit aliquid quam provident reprehenderit debitis facilis consequatur ipsam non ullam, commodi exercitationem quas!</p1>
     </div>
   )
 }
